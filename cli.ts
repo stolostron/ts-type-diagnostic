e134e48fa89596bcd87566b0030ac830cfa5292a
John Swanke <jswanke@redhat.com>
2023-05-31 17:21:39 -0400

    reorg
    Signed-off-by: John Swanke <jswanke@redhat.com>

--- cli.ts
+++ cli.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-import { startSniffing } from './src/theHound'
+import { startSniffing } from './utils/theHound'
 
-let isVerbose
+let isVerbose: boolean = false
 const fileNames = process.argv.slice(2).filter((arg) => {
   if (arg.startsWith('-')) {
     if (arg.startsWith('-v') || arg.startsWith('--v')) isVerbose = true
